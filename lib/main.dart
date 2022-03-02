08316b543ca750165d0f302803a857441c9139b9
Pratik Pandey <pratiik.pandey@gmail.com>
2022-03-02 15:19:24 +0530

    Building Screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_web_dashboard/screens/home.dart';
-import 'package:get/get.dart';  //we are converting material app to get material app because we want to change screens without the build context
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: "Dash",
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeScreen(),
+          scaffoldBackgroundColor: Colors.white,
+          textTheme: GoogleFonts.mulishTextTheme(Theme.of(context).textTheme)
+              .apply(bodycolor: Colors.black)),
+      home: Container(),
     );
   }
 }
