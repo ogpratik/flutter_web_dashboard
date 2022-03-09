ca128eb095547e12f8b7d2474c117c5a132a508c
Pratik Pandey <pratiik.pandey@gmail.com>
2022-03-09 17:18:40 +0530

    Update overview_card_medium.dart

---
+++ lib/pages/overview/widgets/overview_card_medium.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'info_card.dart';
+
+class OverviewCardMediumScreenSize extends StatelessWidget {
+  const OverviewCardMediumScreenSize({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.width;
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+          children: [
+            InfoCard(
+              title: "Rides in Progress",
+              value: "7",
+              onTap: () {},
+              topColor: Colors.orange,
+            ),
+            SizedBox(
+              width: _width / 64,
+            ),
+            InfoCard(
+              title: "Package Delivered",
+              value: "17",
+              onTap: () {},
+              topColor: Colors.lightGreen,
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            InfoCard(
+              title: "Cancelled Delivery",
+              value: "3",
+              onTap: () {},
+              topColor: Colors.redAccent,
+            ),
+            SizedBox(
+              width: _width / 64,
+            ),
+            InfoCard(
+              title: "Scheduled Delivered",
+              value: "4",
+              onTap: () {},
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
